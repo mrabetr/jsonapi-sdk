b321b4af06636069bc5ff967001b399b50e012f3
rafik mrabet <mrabetr@hotmail.com>
2022-06-10 17:47:17 +0100

    added deserialize function in src/index.ts

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,11 @@
-describe("basic test", () => {
-   it("should pass", () => {
-       expect(true).toBe(true)
-   })
+import { deserialize } from "../src/index";
+
+describe('deserialize', () => {
+  it("should return a JS resource object with 'id' and 'type' properties", () => {
+    const jsonApiObject = ''
+    const deserialized = deserialize(jsonApiObject)
+
+    expect(deserialized.hasOwnProperty('id')).toBe(true)
+    expect(deserialized.hasOwnProperty('type')).toBe(true)
+  })
 })
