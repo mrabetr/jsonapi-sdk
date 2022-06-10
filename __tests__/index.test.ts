20cdcf3234a2235d1d476c8c661e0078224dc99a
rafik mrabet <mrabetr@hotmail.com>
2022-06-11 00:17:03 +0100

    testing that deserialize returns the same attributes as the JSON:API resource object

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,7 +2,7 @@ import * as JSONAPI from 'jsonapi-typescript';
 import { deserialize } from "../src/index";
 
 describe('deserialize', () => {
-  let jsonApiObject: JSONAPI.Document = {
+  let resourceObject: JSONAPI.Document = {
     data: {
       type: "product",
       id: "1"
@@ -10,16 +10,36 @@ describe('deserialize', () => {
   };
 
   it("should return a JS resource object with 'type' and 'id' properties", () => {
-    const deserialized = deserialize(jsonApiObject)
+    const deserialized = deserialize(resourceObject)
 
     expect(deserialized.hasOwnProperty('type')).toBe(true)
     expect(deserialized.hasOwnProperty('id')).toBe(true)
   })
 
   it("should return a JS resource object with type 'product' and id '1'", () => {
-    const deserialized = deserialize(jsonApiObject)
+    const deserialized = deserialize(resourceObject)
 
     expect(deserialized.type).toBe('product')
     expect(deserialized.id).toBe('1')
+  })
+
+  describe('of a resource object with attributes', () => {
+    resourceObject = {
+      data: {
+        type: "product",
+        id: "1",
+        attributes: {
+          name: "iPhone 13 Pro Silver 128GB",
+          sku: "iphone-13-pro-silver-128gb"
+        }
+      }
+    };
+
+    it("should return a resource with the attributes as properties", () => {
+      const deserialized = deserialize(resourceObject)
+
+      expect(deserialized.hasOwnProperty('name')).toBe(true)
+      expect(deserialized.hasOwnProperty('sku')).toBe(true)
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import * as JSONAPI from 'jsonapi-typescript';
+// import { utilFunction } from "./utils";
 
 interface Resource {
   type: string
   id: string
+  name: string
+  sku: string
 }
 
-export function deserialize(jsonApiObject: JSONAPI.Document): Resource {
+export function deserialize(resourceObject: JSONAPI.Document): Resource {
   return {
     type: "product",
-    id: "1"
+    id: "1",
+    name: "iPhone 13 Pro Silver 128GB",
+    sku: "iphone-13-pro-silver-128gb"
   }
 }
